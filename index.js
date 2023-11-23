923dd766e25c42f8f525331a1aa9745aba578658
Md Saayed <mdsayed.contact@gmail.com>
2023-11-23 13:10:16 +0600

    Added the menu api

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
 
 
 app.use(express.json());
 app.use(cors());
+
+// own midlewares
+const varifyToken = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 
 
@@ -33,10 +50,44 @@ async function run() {
         const reviewCollection = client.db("bistroDb").collection("reviews");
         const cartCollection = client.db("bistroDb").collection("carts");
 
+        // varify admin
+        const varifyAdmin = async (req, res, next) => {
+            const email = req.decoded?.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
         // user related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', varifyToken, varifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
+        })
+
+        // varify admin
+        app.get('/users/admin/:email', varifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
         })
 
         // post users
@@ -52,7 +103,7 @@ async function run() {
         })
 
         // admin related api
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', varifyToken, varifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
@@ -65,9 +116,9 @@ async function run() {
         })
 
         // delete user
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id', varifyToken, varifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
@@ -77,6 +128,14 @@ async function run() {
             const cursor = await menuCollection.find().toArray();
             res.send(cursor);
         })
+
+        // menu item post 
+        app.post('/menu', varifyToken, varifyAdmin, async (req, res) => {
+            const item = req.body;
+            const result = await menuCollection.insertOne(item);
+            res.send(result);
+        })
+
         app.get('/reviews', async (req, res) => {
             const cursor = await reviewCollection.find().toArray();
             res.send(cursor);
